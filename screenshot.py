29038a2a4cc92842f3f664d38a6ed46678e5459e
soloys <oystein.solaas@gmail.com>
2024-02-25 16:40:04 +0100

    Add TextClip to create video clips

--- screenshot.py
+++ screenshot.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+import time
 
 # Config
 screenshotDir = "Screenshots"
@@ -10,7 +12,7 @@ screenHeight = 800
 
 def getPostScreenshots(filePrefix, script):
     print("Taking screenshots...")
-    driver, wait = __setupDriver(script.url)
+    driver, wait = __setupDriver(script.url)  # 'Accept all' button and cross marker are handled here
     script.titleSCFile = __takeScreenshot(filePrefix, driver, wait)
     for commentFrame in script.frames:
         commentFrame.screenShotFile = __takeScreenshot(filePrefix, driver, wait, f"t1_{commentFrame.commentId}")
@@ -18,7 +20,13 @@ def getPostScreenshots(filePrefix, script):
 
 def __takeScreenshot(filePrefix, driver, wait, handle="Post"):
     method = By.CLASS_NAME if (handle == "Post") else By.ID
-    search = wait.until(EC.presence_of_element_located((method, handle)))
+
+    try:
+        search = wait.until(EC.presence_of_element_located((method, handle)))
+    except TimeoutException:
+        print(f"Element with {method} '{handle}' not found on the page")
+        return
+
     driver.execute_script("window.focus();")
 
     fileName = f"{screenshotDir}/{filePrefix}-{handle}.png"
@@ -36,5 +44,17 @@ def __setupDriver(url: str):
 
     driver.set_window_size(width=screenWidth, height=screenHeight)
     driver.get(url)
+
+    try:
+        button = wait.until(EC.element_to_be_clickable((By.XPATH, "//*[contains(text(),'Accept all')]")))
+        button.click()
+    except:
+        print("No 'Accept all' button found on the page")
+
+    try:
+        cross_marker = wait.until(EC.element_to_be_clickable((By.XPATH, "//*[name()='path' and @d='M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z']")))
+        cross_marker.click()
+    except:
+        print("No cross marker found on the page")
 
     return driver, wait
